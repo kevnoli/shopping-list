9fff97715920cde3f3eb918c1dcb92fca3b9e71f
Kevin Oliveira <kevinols08@gmail.com>
2023-01-25 18:13:34 -0300

    add currency input

---
+++ frontend/src/components/VCurrencyInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-text-field v-model="formattedValue" ref="inputRef"></v-text-field>
+</template>
+<script>
+import { useCurrencyInput } from "vue-currency-input";
+
+export default {
+  name: "VCurrencyInput",
+  props: {
+    modelValue: Number,
+    options: Object,
+  },
+  setup(props) {
+    const options = {
+      "locale": "en-US",
+      "currency": "USD",
+      "currencyDisplay": "hidden",
+      "valueRange": {
+        "min": 0
+      },
+      "precision": 2,
+      "autoDecimalDigits": true,
+      "useGrouping": false,
+      "accountingSign": false
+    }
+    const { inputRef, formattedValue } = useCurrencyInput(props.options || options);
+
+    return { inputRef, formattedValue };
+  },
+};
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -13,11 +13,14 @@ import { createApp } from 'vue'
 // Plugins
 import { registerPlugins } from '@/plugins'
 import axios from '@/plugins/axios'
+import VCurrencyInput from '@/components/VCurrencyInput'
 
 const app = createApp(App)
 
 registerPlugins(app)
 
 app.provide('axios', axios)
+
+app.component('VCurrencyInput', VCurrencyInput)
 
 app.mount('#app')
