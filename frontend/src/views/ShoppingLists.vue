46638193e911f512876c5947e41adcae19305e7e
Kevin Oliveira <kevinols08@gmail.com>
2023-01-30 17:30:27 -0300

    add create list button

--- frontend/src/views/ShoppingLists.vue
+++ frontend/src/views/ShoppingLists.vue
@@ -1,12 +1,40 @@
 <template>
   <v-card>
-    <v-card-title> Minhas listas </v-card-title>
+    <v-card-title>
+      <div class="d-flex">
+        Minhas listas
+        <v-spacer />
+        <v-dialog v-model="dialog">
+          <template #activator="{ props }">
+            <v-btn v-bind="props" icon="mdi-plus" variant="flat" />
+          </template>
+          <v-card>
+            <v-card-title>
+              Add list
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col>
+                    <v-text-field v-model="list.name" label="Name" variant="underlined" />
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn color="primary" @click="saveList()">Salvar</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </div>
+    </v-card-title>
     <v-card-text>
       <v-data-table :headers="headers" :items="lists" @click:row.prevent="openDetails">
-        <template v-slot:item.created_at="{ item }">
+        <template #item.created_at="{ item }">
           {{ dayjs(item.raw.created_at).format("YYYY-MM-DD") }}
         </template>
-        <template v-slot:item.updated_at="{ item }">
+        <template #item.updated_at="{ item }">
           {{ dayjs(item.raw.updated_at).format("YYYY-MM-DD") }}
         </template>
       </v-data-table>
@@ -21,18 +49,27 @@ import { useRouter } from "vue-router";
 const router = useRouter();
 const axios = inject("axios");
 
+const dialog = ref(false)
 const lists = ref([]);
+const list = ref({});
 const headers = [
   { key: "name", title: "Name" },
   { key: "created_at", title: "Created at" },
-  { key: "updated_at", title: "Updated at" }]
+  { key: "updated_at", title: "Updated at" },
+]
 
-function formatDate(date) {
-  return dayjs(date).format("YYYY/MM/DD");
+function openDetails(ev, data) {
+  return router.push(`/shopping-list/${data.item.raw.id}`);
 }
 
-function openDetails(item, data) {
-  return router.push(`/shopping-list/${data.item.raw.id}`);
+function saveList() {
+  axios
+    .post("/shopping-lists", list.value)
+    .then((resp) => {
+      list.value = {}
+      lists.value.push(resp.data)
+      dialog.value = false
+    })
 }
 
 onMounted(() => {
